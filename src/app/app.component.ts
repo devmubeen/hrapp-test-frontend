d192c8756342c56a6e2ddf1ee95ec8419e3390b5
Mubeen Rafiq <devmubeenrafiq@gmail.com>
2018-08-05 18:33:35 +0500

    Initial Build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  employeeForm: FormGroup;
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+  END_POINT = "http://localhost:5000/api/";
+  headers: Headers = new Headers();
+  options: any;
+
+  branches = [];
+  employees = [];
+
+  constructor(private fb: FormBuilder, private http: Http) {
+
+    this.headers.append('Content-Type', 'application/json');
+    this.options = new RequestOptions();
+    this.options.headers = this.headers;
+   }
+
+  ngOnInit() {
+
+
+    this.employeeForm = this.fb.group({
+      'firstname': ['', Validators.required],
+      'lastname': ['', Validators.compose([Validators.required])],
+      'email' : ['', Validators.pattern(this.emailPattern)],
+      'password' : ['', Validators.required],
+      'branch': ['DEFAULT', Validators.required]
+    });
+
+
+    this.getBranches();
+    this.getEmployees();
+
+   
+
+  }
+
+
+  getBranches(){
+
+     this.http.get(this.END_POINT + 'branch', this.options)
+    .subscribe((response: Response) => {
+
+      this.branches = response.json().data; 
+      console.log(this.branches);
+
+    });
+
+
+  }
+
+
+  addEmployee(values){
+
+    console.log(values);
+    this.http.post(this.END_POINT + 'emp',values, this.options)
+    .subscribe((response: Response) => {
+        let resp = response.json();
+        console.log(resp);
+        if(resp.success == true){
+          this.getEmployees();
+          this.employeeForm = this.fb.group({
+            'firstname': ['', Validators.required],
+            'lastname': ['', Validators.compose([Validators.required])],
+            'email' : ['', Validators.pattern(this.emailPattern)],
+            'password' : ['', Validators.required],
+            'branch': ['DEFAULT', Validators.required]
+          });
+
+        }
+        else{
+
+          alert('Error Occured');
+        }
+        
+
+    });
+    
+  }
+
+
+  getEmployees(){
+
+    this.http.get(this.END_POINT + 'emp', this.options)
+    .subscribe((response: Response) => {
+
+      this.employees = response.json().data; 
+      console.log(this.employees);
+
+    });
+
+
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
